8d67e9ec78457936aed4496f45671128f58e625e
eliaszabaneh <elias.zabaneh@gmail.com>
2022-04-12 15:05:01 +0400

    Update 2 12042022 update

--- gmailPyProject/gmail.py
+++ gmailPyProject/gmail.py
@@ -14,9 +14,7 @@ SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
 
 
 def main():
-    """Shows basic usage of the Gmail API.
-    Lists the user's Gmail labels.
-    """
+    global msg
     creds = None
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
@@ -38,12 +36,13 @@ def main():
     try:
         # Call the Gmail API
         service = build('gmail', 'v1', credentials=creds)
-        # results = service.users().labels().list(userId='me').execute()
-        # labels = results.get('labels', [])
 
         # Get Messages
         results = service.users().messages().list(userId='me', labelIds=['SPAM']).execute()
         messages = results.get('messages', [])
+        print("Found {} messages".format(len(messages)))
+        # print("Messages type: ", type(messages), "length: ", len(messages))
+        # return
 
         message_count = int(input("How many messages do you want to see? "))
         if not messages:
@@ -54,10 +53,35 @@ def main():
             for message in messages[:message_count]:
                 # print(message)
                 message_id = message['id']
-                message = service.users().messages().get(userId='me', id=message_id).execute()
-                # print(message)
-                print("Message ID: {}".format(message['id']))
-                print(message['snippet'])
+                # return
+
+                msg = service.users().messages().get(userId='me', id=message_id).execute()
+                ## Message keys:  dict_keys(['id', 'threadId', 'labelIds', 'snippet', 'payload', 'sizeEstimate', 'historyId', 'internalDate'])
+                # print("*******Message keys: ", msg.keys())
+                # #Message['payload'] keys:  dict_keys(['partId', 'mimeType', 'filename', 'headers', 'body', 'parts'])
+                # print("*******Message[\'payload\'] keys: ", msg['payload'].keys())
+                # print("*******Message[\'payload\'][\'headers\'] type: ", type(msg['payload']['headers']), " length: ", len(msg['payload']['headers']))
+                # print("*******Message[\'payload\'][\'headers\'] 0: ", msg['payload']['headers'][0])
+                # print("*******Message[\'payload\'][\'headers\'] 1: ", msg['payload']['headers'][1])
+                # print("*******Message[\'payload\'][\'headers\'] 2: ", msg['payload']['headers'][2])
+                print("******* Start: Message Payload Headers **************************")
+                for vals in msg['payload']['headers']:
+                    # print("vals", vals)
+                    # print("vals[name]", vals['name'])
+                    # print("vals[value]", vals['value'])
+
+                    if vals['name'] == 'From':
+                        print("## From: ", vals['value'])
+                    if vals['name'] == 'Delivered-To':
+                        print("## Delivered-To : ", vals['value'])
+                    if vals['name'] == 'Subject':
+                        print("## Subject: ", vals['value'])
+                    # print(vals['name'], ": ", vals['value'])
+                print("******* End: Message Payload Headers **************************")
+
+                print("Message ID: {}".format(msg['id']))
+                # print("Message ID: {}".format(message_id))
+                # print(msg['snippet'])
                 print("-----------------------------------------------------------")
                 time.sleep(2)
 
